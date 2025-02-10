4e17ddfeec6f47c688c181145398b3380128fbbf
mohamed-mo9 <mohamedrahoma81@gmail.com>
2025-02-11 00:14:23 +0200

    commited successfully

---
+++ inventory.js
@@ -0,0 +1,38 @@
+/*
+    Inventory Application
+inputs: item1,item2,item3
+processing: 
+ - create object to represent the inventory
+ - create function to add the items => constructor
+ - create function to remove items
+ - create function to list items
+output: export this module in cjs to other file
+*/
+
+// function Inventory(Id, Name, Price, Quantity)
+// {
+//   this.id = Id;
+//   this.name = Name;
+//   this.price = Price;
+//   this.quantity = Quantity;
+
+//   this.listItems = function()
+//   {
+     
+//   }
+// }
+
+// let Item1 = new Inventory("Mobile", "Samsung", 15000, 12);
+// let Item2 = new Inventory("Mobile", "Xiaomi", 5000, 20);
+// let Item3 = new Inventory("Laptop", "HP", 10000, 10);
+// let Item4 = new Inventory("Laptop", "DELL", 8000, 15);
+
+
+// const Inventory = [
+//   {id: "Mobile", Name: "Samsung", price: 5000, Quantity: 20},
+//   {id: "laptop", Name: "HP", price: 10000, Quantity: 10},
+//   {id: "PowerBank", Name: "Energizer", price: 500, Quantity: 2},
+// ];
+
+let date = new Date("1970-01-02");
+console.log(date * 1);
